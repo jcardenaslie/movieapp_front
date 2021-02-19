7c72d953641eadde3656389873fc320c60948a88
Joaquin Cardenas <joaquin.cardenas@almacenguru.com>
2021-02-19 02:05:08 -0300

    Update omdb api variable name

--- services/openapi.js
+++ services/openapi.js
@@ -1,20 +1,20 @@
-const OPEN_API_URL = "https://www.omdbapi.com/?i=tt3896198&apikey=24b0afcf"
+const OMDB_API_URL = "https://www.omdbapi.com/?i=tt3896198&apikey=24b0afcf"
 
 
 const getPage = async(movie, page) => {
-  const result = await fetch(`${OPEN_API_URL}&s="${movie}"&page=${page}`)
+  const result = await fetch(`${OMDB_API_URL}&s="${movie}"&page=${page}`)
   const data = await result.json()
   return data.Search
 }
 
 const getMovieDetail = async (id) => {
-  const result = await fetch(`${OPEN_API_URL}&i="tt0076759"&plot=full`)
+  const result = await fetch(`${OMDB_API_URL}&i="tt0076759"&plot=full`)
   const data = await result.json()
   return data
 }
 
 const getMovieDetailByTitle = async (title) => {
-  const result = await fetch(`${OPEN_API_URL}&t="${title}"`)
+  const result = await fetch(`${OMDB_API_URL}&t="${title}"`)
   const data = await result.json()
   return data
 }
